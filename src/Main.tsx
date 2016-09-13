7759425765a8c655e34b5edd1368ed0164db41f0
Matt Gadd <mgadd@sitepen.com>
2016-09-13 17:16:59 +0100

    add spinner component

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,18 +1,37 @@
 import * as React from 'react';
 
-export default class Login extends React.Component<{}, {}> {
+class Spinner extends React.Component<{}, {}> {
 	public render() {
+		return (
+			<div className='spinner'>
+				<div className='dot1'></div>
+				<div className='dot2'></div>
+			</div>
+		);
+	}
+}
+
+export default class Login extends React.Component<{}, {
+	loading: boolean
+}> {
+	constructor(props: any) {
+		super(props);
+		this.state = { loading: true };
+		setTimeout(() => this.setState({ loading: false }), 3000);
+	}
+	public render() {
+		let content = <div>Welcome to My Application</div>;
+		if (this.state.loading) {
+			content = <Spinner />;
+		}
 		return (
 			<div className='row col-md-6 col-md-offset-3'>
 				<div className='panel panel-default'>
 					<div className='panel-heading'>
-						<h3 className='panel-title'>Hello World</h3>
+						<h3 className='panel-title'>My Application</h3>
 					</div>
 					<div className='panel-body main'>
-						<div className='spinner'>
-							<div className='dot1'></div>
-							<div className='dot2'></div>
-						</div>
+						{ content }
 					</div>
 				</div>
 			</div>
