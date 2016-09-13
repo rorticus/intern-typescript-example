5a9726b5ec9369694ecee68df6a3d735e71442a8
Matt Gadd <mgadd@sitepen.com>
2016-09-13 11:16:12 +0100

    add react

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-const div = document.createElement('div');
-const helloWorld = document.createElement('span');
-helloWorld.classList.add('world');
-helloWorld.innerHTML = 'hello world';
-div.appendChild(helloWorld);
-div.classList.add('main');
-document.body.appendChild(div);
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Login from './Login';
+
+ReactDOM.render(
+	React.createElement(Login, { foo: 'bar' }),
+	document.getElementById('app')
+);
