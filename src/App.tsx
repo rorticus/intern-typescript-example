0e3ea07173c8002f4e0dd44453c383aff64bfb0c
Matt Gadd <mgadd@sitepen.com>
2016-09-13 16:38:33 +0100

    add main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Login from './Login';
+import Main from './Main';
 
 export default class App extends React.Component<{}, {
 	loggedIn: boolean
@@ -7,11 +8,12 @@ export default class App extends React.Component<{}, {
 	constructor(props: any) {
 		super(props);
 		this.state = { loggedIn: false };
+		setTimeout(() => this.setState({ loggedIn: true }), 3000);
 	}
 	public render() {
 		let main = <Login />;
 		if (this.state.loggedIn) {
-			main = <div>Hello World</div>;
+			main = <Main />;
 		}
 		return (
 			<div className='container'>
---
+++ src/Main.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+export default class Login extends React.Component<{}, {}> {
+	public render() {
+		return (
+			<div className='row col-md-6 col-md-offset-3'>
+				<div className='panel panel-default'>
+					<div className='panel-heading'>
+						<h3 className='panel-title'>Hello World</h3>
+					</div>
+					<div className='panel-body'>
+						<div>Hello World</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
