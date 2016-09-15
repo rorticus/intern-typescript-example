bcb50f34909505a63c0f2d5ddb6fd60305e4d6fe
Matt Gadd <mgadd@sitepen.com>
2016-09-15 17:36:38 +0100

    remove redux

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,7 @@ require.config({
 	baseUrl: '../../',
 	packages: [
 		{ name: 'react', location: '../node_modules/react/dist', main: 'react' },
-		{ name: 'react-dom', location: '../node_modules/react-dom/dist', main: 'react-dom' },
-		{ name: 'redux', location: '../node_modules/redux/dist', main: 'redux' },
-		{ name: 'react-redux', location: '../node_modules/react-redux/dist', main: 'react-redux' }
+		{ name: 'react-dom', location: '../node_modules/react-dom/dist', main: 'react-dom' }
 	]
 });
 
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
 import { createElement } from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducer';
 import App from './App';
 
-const store = createStore(reducer);
-
 render(
-	createElement(
-		Provider,
-		{ store: store },
-		createElement(App, null)
-	),
+	createElement(App, null),
 	document.getElementById('app')
 );
